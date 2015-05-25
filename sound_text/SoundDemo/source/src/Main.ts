d8c369c0a2b544c59e5eb1d7f3b9bd1a35787d14
yangxiao <yangxiao@egret.com>
2015-05-25 12:01:25 +0800

    增加新的音乐示例库

--- sound_text/SoundDemo/source/src/Main.ts
+++ sound_text/SoundDemo/source/src/Main.ts
@@ -1,35 +1,12 @@
-/**
- * Copyright (c) 2014,Egret-Labs.org
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Egret-Labs.org nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY EGRET-LABS.ORG AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL EGRET-LABS.ORG AND CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+﻿/**
+ *  声音演示示例
  */
 
 class Main extends egret.DisplayObjectContainer {
 
     /**
      * 加载进度界面
-     * loading process interface
+     * Process interface loading
      */
     private loadingView: LoadingUI;
 
@@ -39,36 +16,31 @@ class Main extends egret.DisplayObjectContainer {
     }
 
     private onAddToStage(event: egret.Event) {
-        //inject the custom material parser
-        //注入自定义的素材解析器
-        egret.Injector.mapClass("egret.gui.IAssetAdapter", AssetAdapter);
-        // load skin theme configuration file, you can manually modify the file. And replace the default skin.
-        //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
-        egret.gui.Theme.load("resource/theme.thm");
-        //Config loading process interface
         //设置加载进度界面
+        //Config to load process interface
         this.loadingView = new LoadingUI();
         this.stage.addChild(this.loadingView);
-        // initialize the Resource loading library
+
         //初始化Resource资源加载库
+        //initiate Resource loading library
         RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.loadConfig("resource/resource.json", "resource/");
     }
     /**
      * 配置文件加载完成,开始预加载preload资源组。
-     * Loading of configuration file is complete, start to pre-load the preload resource group
+     * configuration file loading is completed, start to pre-load the preload resource group
      */
     private onConfigComplete(event: RES.ResourceEvent): void {
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
-        RES.loadGroup("preload",0);
+        RES.loadGroup("preload");
         RES.loadGroup("soundload",1);
     }
     /**
      * preload资源组加载完成
-     * preload resource group is loaded
+     * Preload resource group is loaded
      */
     private onResourceLoadComplete(event: RES.ResourceEvent): void {
         if (event.groupName == "preload") {
@@ -76,23 +48,23 @@ class Main extends egret.DisplayObjectContainer {
             RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
             RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
             RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
-            this.createScene();
+            this.createGameScene();
         }
     }
     /**
     * 资源组加载出错
-     * Resource group loading failed
+     *  The resource group loading failed
     */
     private onResourceLoadError(event: RES.ResourceEvent): void {
         //TODO
         console.warn("Group:" + event.groupName + " has failed to load");
         //忽略加载失败的项目
-        //ignore loading failed projects
+        //Ignore the loading failed projects
         this.onResourceLoadComplete(event);
     }
     /**
      * preload资源组加载进度
-     * loading process of preload resource
+     * Loading process of preload resource group
      */
     private onResourceProgress(event: RES.ResourceEvent): void {
         if (event.groupName == "preload") {
@@ -100,42 +72,50 @@ class Main extends egret.DisplayObjectContainer {
         }
     }
 
-    private gameLayer: egret.DisplayObjectContainer;
-
-    private guiLayer: egret.gui.UIStage;
+    private textContainer: egret.Sprite;
     /**
-     * 创建场景界面
-     * Create scene interface
+     * 创建游戏场景
+     * Create a game scene
      */
-    private createScene(): void {
+    private createGameScene(): void {
 
-        //游戏场景层，游戏场景相关内容可以放在这里面。
-        //Game scene layer, the game content related to the scene can be placed inside this layer.
-        this.gameLayer = new egret.DisplayObjectContainer();
-        this.addChild(this.gameLayer);
-        //var bitmap: egret.Bitmap = new egret.Bitmap();
-        //bitmap.texture = RES.getRes("bgImage");
-        //this.gameLayer.addChild(bitmap);
+        var container:egret.Sprite = new egret.Sprite();
+        this.addChild(container);
+        var currentChild;
 
-        //GUI的组件必须都在这个容器内部,UIStage会始终自动保持跟舞台一样大小。
-        //GUI components must be within the container, UIStage will always remain the same as stage size automatically.
-        this.guiLayer = new egret.gui.UIStage();
-        this.addChild(this.guiLayer);
+        container.x = 150;
+        container.y = 50;
+        container.graphics.beginFill(0xffffff, 1);
+        container.graphics.drawRect(0, 0, 200, 50);
+        container.graphics.endFill();
 
-        /*
-        var showcase:Showcase = new Showcase();
-        //在GUI范围内一律使用addElement等方法替代addChild等方法。
-        //Within GUI scope, addChild methods should be replaced by addElement methods.
-        this.guiLayer.addElement(showcase);*/
-
-        //创建AudioPlayer用来播放音乐。
-        var audioplayer:AudioPlayer = new AudioPlayer();
-        //在GUI范围内一律使用addElement等方法替代addChild等方法。
-        //Within GUI scope, addChild methods should be replaced by addElement methods.
-        this.guiLayer.addElement(audioplayer);
-
+        var text:egret.TextField = new egret.TextField();
+        this.addChild(text);
+        text.x = 230;
+        text.y = 65;
+        text.text = "Play";
+        text.textColor = 0xff0000;
+        text.size = 20;
+        text.touchEnabled = true;
+        text.addEventListener(egret.TouchEvent.TOUCH_TAP, this.btnTouchHandler, text);
     }
 
+    private btnTouchHandler(evt:egret.TouchEvent):void {
+        var sound:egret.Sound = RES.getRes("sound");
+        sound.type = egret.Sound.MUSIC;
+
+        console.log("button touched");
+
+        //点击按钮，音乐在播放和停止状态间转换
+        if (evt.target.text == "Play") {
+            sound.play();
+            evt.target.text = "Stop";
+        } else {
+            sound.pause();
+            evt.target.text = "Play";
+        }
+
+    }
 
 }
 
